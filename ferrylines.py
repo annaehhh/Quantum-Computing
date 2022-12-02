4222fbbead2c65eaf63ba1e48bc35ab761c52437
annaehhh <97302590+annaehhh@users.noreply.github.com>
2022-12-02 11:29:35 +0100

    Read csv.file into pd.Dataframe

--- ferrylines.py
+++ ferrylines.py
@@ -3,7 +3,7 @@
 # Rights, distribution?
 
 # Import necessary packages.
-
+import pandas as pd
 
 ###
 ### Preprocessing
@@ -13,10 +13,13 @@
 #   1. harbour names
 #   2. shortest ferry connection
 
+df_harbours = pd.read_csv("data_harbours.csv")
+print(df_harbours.head())
+
 #Input
 #Entering departure and arrival harbour
-start=  input(str("Please enter start harbour:"))
-end= input(str("Please enter destination harbour:"))
+    #start=  input(str("Please enter start harbour:"))
+    #end= input(str("Please enter destination harbour:"))
 
 #Input validation
 # 1. Check whether start and end harbour is included in the input data
