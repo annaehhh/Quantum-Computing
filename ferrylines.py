e2c8ddeda5c4c66e4f6cddd9dec4d357f0496634
annaehhh <97302590+annaehhh@users.noreply.github.com>
2022-12-05 21:22:00 +0100

    Preprocessing: Changing data format

--- ferrylines.py
+++ ferrylines.py
@@ -10,20 +10,29 @@ import pandas as pd
 ###
 
 # Read .csv file containing information on
-#   1. harbour names
+#   1. harbour names (from=column1, to=line1)
 #   2. shortest ferry connection
 
-df_harbours = pd.read_csv("data_harbours_utf8.csv")
-print(df_harbours.columns)
+df_harbours = pd.read_csv("data_harbours_utf8.csv", sep = ";")
 
-#change format to the following columns
-#    1. from
-#    2. to
-#    3. shortest direct distance
 
-#from harbours
-df_from = df_harbours["Column1"]
-df_from.head()
+#change format the following columns in dataframe connections
+#    1. From
+#    2. To
+#    3. Distance
+
+#rename 'Column1' to 'From'
+df_harbours=df_harbours.rename(columns={'Column1':"From"})
+
+#melt data
+df_connections= pd.melt(df_harbours,id_vars=['From'],var_name="To",value_name='Distance')
+print(df_connections.columns)
+
+#filter NaN-values
+df_connections=df_connections[df_connections["Distance"].notnull()]
+#filter out 0.0 values
+df_connections=df_connections[df_connections.Distance != 0]
+print(df_connections)
 
 #Input
 #Entering departure and arrival harbour
