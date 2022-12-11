d3b42aa97e5292ef1d21fd338d1cdb47fc56d874
annaehhh <97302590+annaehhh@users.noreply.github.com>
2022-12-11 14:22:24 +0100

    BQM: Uniting existing files to create class Ferry

--- ferrylines.py
+++ ferrylines.py
@@ -45,3 +45,90 @@ destination= input(str("Please enter destination harbour:"))
 
 #Input validation
 # 1. Check whether departure and destination harbour is included in the input data
+
+###
+###
+###
+
+from dwave.system.samplers import DWaveSampler
+from dwave.system.composites import EmbeddingComposite
+import dwave.inspector
+import dwavebinarycsp
+
+class Ferry:
+    def __init__(self):
+        self.routes = [
+        ["Hamburg"  , "Helgoland", 20],
+        ["Hamburg"  , "Romo", 60],
+        ["Romo"     , "Esberg", 20],
+        ["Helgoland", "Esberg", 10],
+        #["Hamburg"  , "London", 10],
+        #["Start" , "Hamburg",0],
+        #["Esberg", "Ende",0]
+        ]
+        self.csp = dwavebinarycsp.ConstraintSatisfactionProblem(dwavebinarycsp.BINARY)
+        self.ports = set([])
+        self.n=0
+        for route in self.routes:
+            self.ports.add(route[0])
+            self.ports.add(route[1])
+
+    def get_label(self,start, end, length):
+        return "{start},{end}{length}".format(**locals())
+
+    def sum_to_two_or_zero(self,*args):
+        fromList=[]
+        toList=[]
+        x = 0
+        print(args)
+        for a in args:
+            if x != self.n:
+                toList.append(a)
+                x=x+1
+            else:
+                fromList.append(a)
+
+        sum_value0 = sum(fromList)
+        sum_value1 = sum(toList)
+        return ((sum_value0 == 1 and sum_value1 == 1) or (sum_value0 == 0 and sum_value1 == 0))
+
+    def get_bqm(self):
+        print(self.ports)
+        for port in self.ports:
+            directions = []
+            self.n=0
+            for route in self.routes:
+                if (route[0]==port):
+                    directions.append("to"+self.get_label(route[0],route[1],route[2]))
+                self.n = self.n+1
+            if (port=="Hamburg"):
+                self.n = self.n+1
+                directions.append("toStart,Hamburg0")
+            if (port=="Esberg"):
+                directions.append("fromEnde,Esberg0")
+            for route in self.routes:
+                if (route[1]==port):
+                    directions.append("from"+self.get_label(route[1],route[0],route[2])) 
+            self.csp.add_constraint(self.sum_to_two_or_zero, directions)
+        print(self.csp.constraints)
+        self.csp.add_variable("toStart,Hamburg0")
+        self.csp.add_variable
+        self.csp.fix_variable("toStart,Hamburg0",1)
+        self.csp.fix_variable("fromEnde,Esberg0",1)
+        bqm = dwavebinarycsp.stitch(self.csp)
+        return bqm
+
+
+f = Ferry()
+
+bqm=f.get_bqm()
+
+print(bqm.to_polystring())
+
+#sampler = EmbeddingComposite(DWaveSampler())
+#result = sampler.sample(bqm,
+#                        num_reads=100,
+#                        chain_strength=1,
+#                        label='Example - Maze')
+#
+#dwave.inspector.show(result)  
